5adab5444816ad8a0d8ac78a9996628282ff0da0
taityo <taicho.nv@gmail.com>
2018-11-25 00:25:14 +0900

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,17 @@
 # $0:hostname $1:ip adress
 
 # ros network
-~/.bashrc << ROS_MASTER_URI=http://$1:11311
-~/.bashrc << ROS_IP=$1
-~/.bashrc << ROS_HOSTNAME=$0
+~/.bashrc << export ROS_MASTER_URI=http://$1:11311
+~/.bashrc << export ROS_IP=$1
+~/.bashrc << export ROS_HOSTNAME=$0
+~/.bashrc << export ROSLAUNCH_SSH_UNKNOWN=1
 source ~/.bashrc
 
 # hostname
 /etc/hosts << $0 $1
 
 # ntp server
+ntpdate ntp.nict.jp
 
 # package install
 apt-get install ros-kinetic-navigation
